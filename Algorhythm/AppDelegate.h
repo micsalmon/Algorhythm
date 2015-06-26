83e2d5489f90aa75ff3c25f6e799e6ac6db9af4f
Michael Thomas Salmon <myksalmon@Michaels-MacBook-Air.local>
2015-06-25 23:00:30 -0400

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Michael Thomas Salmon on 6/25/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Michael Thomas Salmon on 6/25/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
